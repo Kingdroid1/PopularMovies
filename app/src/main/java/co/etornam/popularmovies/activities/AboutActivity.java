9b300794fc6a795504d372e34b02ae0afcc9b647
RegNex <sunumacbright@gmail.com>
2018-08-25 13:14:30 +0000

    added about app

--- app/src/main/java/co/etornam/popularmovies/activities/AboutActivity.java
+++ app/src/main/java/co/etornam/popularmovies/activities/AboutActivity.java
@@ -1,7 +1,11 @@
 package co.etornam.popularmovies.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import java.util.Objects;
 
 import co.etornam.popularmovies.R;
 
@@ -11,5 +15,17 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Intent intent = new Intent(AboutActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
